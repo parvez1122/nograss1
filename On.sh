c88f683765479a5dcf28a2f7da73717ee721acfe
parvez1122 <74459985+parvez1122@users.noreply.github.com>
2020-11-14 20:41:25 +0600

    Create On.sh

---
+++ On.sh
@@ -0,0 +1,11 @@
+mkdir /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config
+echo "[SystemSettings]
+r.Fog=AspectRatio_MaintainsYFOV
+
+[/Script/Engine.RendererOverrideSettings]
+r.VSync=1
+grass.heightScale=0
+
+[MainChannel]= Mod_Data
+
+" > /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Config/UserEngine.ini
